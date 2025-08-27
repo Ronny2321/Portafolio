3478b77a0774a2749d812e8e764a279d78f26e46
Ronny2321 <ro.silva@udla.edu.co>
2025-08-26 21:08:50 -0500

    Se mejoro el diseño completo de la pagina web

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,10 @@ const menu = document.getElementById('menu');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
+
+const links = document.querySelectorAll('#menu li a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('show');
+    });
+});
